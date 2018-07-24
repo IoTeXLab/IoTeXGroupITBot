904c6ff77d4fbd0fa5c99057c11dc94b470db90b
@sromano <mail@sromano.com>
2018-07-24 18:58:35 +0200

    Added initial configuration management Added json configuration file with basic spam filter configuration. Added Telegram Bot Api Key configuration both through environment var and through configuration file

--- bot.go
+++ bot.go
@@ -4,14 +4,32 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 	"os"
+	"github.com/tkanos/gonfig"
 )
 
+type Configuration struct {
+	ApiKey string
+	KickOnFirstNameLength bool
+	FirstNameMaxLength int
+	KickOnFullNameLength bool
+	FullNameMaxLength int
+	CommandsWhitelist []string
+}
+
+var configuration Configuration
+
 func main() {
-	API := os.Getenv("BOTAPIKEY")
-	if API == "" {
-		log.Fatal("Telegram Bot API key not defined.\nPlease define environment variable BOTAPIKEY")
-		return
+
+	err := gonfig.GetConf("configuration.json", &configuration)
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	log.Println(configuration)
+
+	API := GetApiKey();
+
 	log.Printf("Found Telegram Bot API: %s", API)
 	bot, err := tgbotapi.NewBotAPI(API)
 	if err != nil {
@@ -27,16 +45,58 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
+	updates.Clear()
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message.IsCommand() {
+			// Process commands here
+		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		if update.Message.NewChatMembers != nil {
+			for _,user:=range *update.Message.NewChatMembers {
+				if configuration.KickOnFirstNameLength && len(user.FirstName) > configuration.FirstNameMaxLength {
+					config:=tgbotapi.KickChatMemberConfig{}
+					config.ChatID = update.Message.Chat.ID
+					config.UserID = user.ID
+					_, err := bot.KickChatMember(config)
+					if err != nil{
+						log.Printf("Error kicking user %s: %s",user.UserName, err)
+					}
+				}
+			}
+		}
 
-		bot.Send(msg)
 	}
+}
+
+func GetApiKey() string {
+	// "673613669:AAFt-sbY8CA67oRpUPCV5O4P5cYQPFhLzM0"
+
+	usingJson := false;
+
+	log.Print("looking for Telegram Bot API Key in BOTAPIKEY environment variable...")
+	API := os.Getenv("BOTAPIKEY")
+	if API == "" {
+		log.Print("Not found\n")
+		log.Print("looking for Telegram Bot API Key in configuration.json file...")
+		API = configuration.ApiKey
+		usingJson = true;
+	}
+
+	if API == "" {
+		log.Fatal("Telegram API Key not found!")
+	}
+	if usingJson {
+		log.Printf("Using Api key found in configuration file:")
+	} else {
+		log.Printf("Using Api key found in Environment variable: ")
+	}
+
+	log.Println(API)
+
+	return API
 }
